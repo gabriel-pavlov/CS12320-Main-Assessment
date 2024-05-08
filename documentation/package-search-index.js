9770efae32c3297553422fdbe0850f1454c289fc
gabriel-pavlov <66558926+gabriel-pavlov@users.noreply.github.com>
2024-05-08 09:47:59 +0100

    further bug fixes and Java docs added

---
+++ documentation/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"gpavlov.cs12320.mainassignment"},{"l":"gpavlov.cs12320.mainassignment.domain"},{"l":"gpavlov.cs12320.mainassignment.persistence"},{"l":"gpavlov.cs12320.mainassignment.ui"}];updateSearchResults();
